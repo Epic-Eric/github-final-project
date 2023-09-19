6e020ff001276b63189717ba2d265ef4999c2f62
Eric Xie <102342311+Epic-Eric@users.noreply.github.com>
2023-09-19 10:41:10 -0700

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,7 +4,7 @@
    # Do not use this in production. Sample purpose only.
    # Author: Upkar Lidder (IBM)
    # Additional Authors:
-   # Epic Eric
+   # Epic-Eric
    # Input:
    # p, principal amount
    # t, time period in years
